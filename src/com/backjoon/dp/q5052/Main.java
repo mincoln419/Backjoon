ec7e4caa294fec3a1d14f153ee063b27524c69f4
mincoln <mincoln419@naver>
2021-10-26 15:14:46 +0900

    trie node

--- src/com/backjoon/dp/q5052/Main.java
+++ src/com/backjoon/dp/q5052/Main.java
@@ -1,66 +1,90 @@
 package com.backjoon.dp.q5052;
 
 import java.io.*;
-import java.util.*;
 
-class Node{
+class TrieNode{
 	
-	int child;
+	TrieNode[] children;
+
+	boolean isEnd;
 	
-	public Node(int child) {
-		this.child = child;
+	public TrieNode() {
+		isEnd = false;
+		children = new TrieNode[10];//0 ~ 9
+		for(int i = 0 ; i < 10; i++) {
+			children[i] = null;
+		}
+	}
+	public void wordEnd() {
+		isEnd = true;
 	}
 }
 
 
 public class Main {
 
-	static List<Node>[] list;
-	static int[] visited;
-	static int k;
-	static List<Integer> answer;
-	static int root;
+	static TrieNode root;
+	
 	public static void main(String[] args) throws Exception {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+	
 		
 		int n = Integer.parseInt(bf.readLine());
-		StringTokenizer st = new StringTokenizer(bf.readLine());
-		list = new List[n];
+	
 		for(int i = 0 ; i < n ;i++) {
-			list[i] = new ArrayList<>();
-		}
-		visited = new int[n];
-		answer = new ArrayList<>();
-		root = -1;
-		for(int i = 0 ; i < n ; i++) {
-			int num = Integer.parseInt(st.nextToken());
-			if(num < 0)root = i;
-			else list[num].add(new Node(i));
-		}
-		k = Integer.parseInt(bf.readLine());
-		
-		if(k == root)System.out.println(0);
-		else {
-			dfs(root);
-			System.out.println(answer.size());
+			int m = Integer.parseInt(bf.readLine());
+			root = new TrieNode();
+			String[] str = new String[m];
+			for(int j = 0 ; j < m ; j++) {
+				str[i] = bf.readLine();
+				
+				insert(str[i]);
+			}
+			
+			boolean chk = true;
+			for(int j = 0 ; j < m; j++) {
+				if(!search(str[i])) {
+ 					chk = false;
+					System.out.println("NO");
+					break;
+				}
+			}	
+			if(chk)System.out.println("YES");
 		}
 	}
 	
-	static void dfs(int node) {
-		visited[node] = 1;
-		boolean chk = true;
-		for(int i = 0 ; i < list[node].size();i++) {
-			int next = list[node].get(i).child;
-			if(next == k)continue;
-			if(visited[next] == 0) {
-				chk = false;
-				visited[next] = 1;
-				dfs(next);
+	static void insert(String key) {
+		int level;
+		int length = key.length();
+		int idx;
+		
+		TrieNode pCrawl = root;
+		
+		for(level = 0; level < length ; level++) {
+			idx = key.charAt(level) - '0';
+			if(pCrawl.children[idx] == null) {
+				pCrawl.children[idx] = new TrieNode();
 			}
+			
+			pCrawl = pCrawl.children[idx];
+		}		
+		
+		pCrawl.wordEnd();
+	}
+	
+	static boolean search(String key) {
+		int level;
+		int length = key.length();
+		int idx;
+		TrieNode pCrawl = root;
+
+		for(level = 0 ; level < length; level++) {
+			idx = key.charAt(level) - '0';
+			
+			if(pCrawl == null || pCrawl.isEnd) return false;
+			pCrawl = pCrawl.children[idx];
 		}
-		if(chk) {
-			//System.out.println("reaf::" + node);
-			answer.add(node);
-		}
+		
+		return (pCrawl != null && pCrawl.isEnd);
 	}
 }
