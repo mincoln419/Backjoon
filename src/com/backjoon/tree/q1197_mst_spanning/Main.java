3a2b45700d84f2df538c0d40171e40cd061a801f
Mermer <mincoln419@naver.com>
2021-12-01 00:25:22 +0900

    kruskal basic

--- src/com/backjoon/tree/q1197_mst_spanning/Main.java
+++ src/com/backjoon/tree/q1197_mst_spanning/Main.java
@@ -27,7 +27,6 @@ public class Main {
 	/*
 	 * 최소 스패닝 트리 : 주어진 그래프의 모든 정점들을 연결하는 부분 그래프 중에서 그 가중치의 합이 최소인 트리를 말한다.
 	 */
-	@SuppressWarnings("unchecked")
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
