deb8a19f6e6940956c5472ee402b3e5590ae1f9b
Mermer <mincoln419@naver.com>
2021-12-16 01:55:37 +0900

    merge sort

--- algorithm-tutoring/src/com/mermer/sort/MergeSort.java
+++ algorithm-tutoring/src/com/mermer/sort/MergeSort.java
@@ -1,47 +1,45 @@
 package com.mermer.sort;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.nio.ByteBuffer;
 import java.util.*;
 
 public class MergeSort {
 
 	//병합정렬 구현
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws IOException {
+		File file = new File("src/com/mermer/sort/target/test.txt");
+		FileOutputStream bw = new FileOutputStream(file);
 		List<Integer> list = new LinkedList<>();
 		
-		for(int i = 0 ; i < 10 ; i ++) {
-			int ran = (int)(Math.random() * 100);
+		for(int i = 0 ; i < 100000 ; i ++) {
+			int ran = (int)(Math.random() * 10000000);
 			if(list.contains(ran))continue;
 			list.add(ran);
 		}
 		
 		//랜덤한 수 10개
-		for(int a : list) System.out.print(a + " ");
-		System.out.println();
+		for(int a : list)bw.write((a + "\n").getBytes());
+		bw.write("\n".getBytes());
 		
-		List<Integer> reult = merge(list);
+		List<Integer> reult = split(list);
 		//정렬한 값
-		for(int a : reult) System.out.print(a + " ");
-		System.out.println();
+		for(int a : reult) bw.write((a + "\n").getBytes());
+		bw.write("\n".getBytes());
+		
+		
+		bw.flush();
+		bw.close();
 	}
 
-	private static List<Integer> merge(List<Integer> list) {
+	private static List<Integer> split(List<Integer> list) {
 		
 		if(list.size() <= 1)return list;
-		
-		List<Integer> result = new ArrayList<>();
-		
-		if(list.size() == 2) {
-			if(list.get(0) > list.get(1)) {
-				result.add(list.get(0));
-				result.add(list.get(1));
-			}else {
-				result.add(list.get(1));
-				result.add(list.get(0));
-			}
-			return result;
-		}
-		
+	
 		int mid = list.size()/2;
 		List<Integer> left = new ArrayList<>();
 		List<Integer> right = new ArrayList<>();
@@ -50,32 +48,36 @@ public class MergeSort {
 			if(i < mid)left.add(list.get(i));
 			else right.add(list.get(i));
 		}
-		merge(left);
-		merge(right);
 		
+		left = split(left);
+		right = split(right);
 		
-		int i = 0, j = 0;
-		while(i + j < list.size()) {
-			int a = left.get(i);
-			int b = right.get(j);
+		return merge(left, right);
+	}
+
+	private static List<Integer> merge(List<Integer> left, List<Integer> right) {
+		List<Integer> result = new ArrayList<>();
+		
+		int i = 0;
+		int j = 0;
+		while(i + j < left.size() + right.size()) {
 			
-			if(a < b) {
-				result.add(a);
+			if(i == left.size()) {
+				result.add(right.get(j));
+				j++;
+			}else if(j == right.size()) {
+				result.add(left.get(i));
 				i++;
 			}else {
-				result.add(b);
-				j++;
-			}
-			
-			if(i == mid - 1) {
-				result.add(b);
-				j++;
-			}else if(j == list.size() - mid) {
-				result.add(a);
-				i++;
+				if(left.get(i) < right.get(j)) {
+					result.add(left.get(i));
+					i++;
+				}else {
+					result.add(right.get(j));
+					j++;
+				}
 			}
 		}
-		
 		return result;
 	}
 
