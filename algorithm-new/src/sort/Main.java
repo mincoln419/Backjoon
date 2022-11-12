168cc0202d3b65e3605f80a6fd67b287f1f22859
mermer vivo <mincoln419@naver.com>
2022-11-12 10:06:01 +0900

    대표값 문제 #2587

--- algorithm-new/src/sort/Main.java
+++ algorithm-new/src/sort/Main.java
@@ -1,9 +1,42 @@
 package sort;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 public class Main {
 
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		
+		
+		int[] arr = IntStream.range(0, 5).map(i -> {
+			try {
+				int n = Integer.parseInt(bf.readLine());
+				return n;
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+			return 0;
+		}).toArray();
+		
+		int avg = (int) Arrays.stream(arr).average().getAsDouble();		
+		bw.write(String.valueOf(avg));
+		bw.write("\n");
+		Arrays.sort(arr);
+		int middle = arr[2];
+		bw.write(String.valueOf(middle));
+		
+		bf.close();
+		bw.flush();
+		bw.close();
 	}
 }
